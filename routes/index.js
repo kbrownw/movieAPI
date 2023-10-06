e2c58154b2731507b5873e7bacdfc11d1c68e421
Keith Brown <kbrownw@gmail.com>
2023-10-06 15:46:06 -0500

    Refactor / route to use local data if in dev environment

--- routes/index.js
+++ routes/index.js
@@ -5,84 +5,71 @@ const fetch = require("node-fetch");
 
 /* GET home page. */
 
-if (process.env.NODE_ENV === "dev") {
-  router.get("/", async function (req, res, next) {
-    const zip = "47905";
-    const url = "http://localhost:3000/theaters/list/" + zip + ".json";
+router.get("/", async function (req, res) {
+  const zip = req.query.zipCode;
+  let url =
+    "https://flixster.p.rapidapi.com/theaters/list?zipCode=" +
+    zip +
+    "&radius=50";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": process.env.FLIXSTER_API_KEY,
+      "X-RapidAPI-Host": "flixster.p.rapidapi.com",
+    },
+  };
 
-    try {
-      const response = await fetch(url);
-      const result = await response.json();
-      console.log(result);
-      res.json(result);
-    } catch (error) {
-      console.error(error);
-      res.json({ error: error.message });
-    }
-  });
+  if (process.env.NODE_ENV) {
+    console.log("Using local data");
+    url = "http://localhost:3000/theaters/list/" + zip + ".json";
+  }
 
-  router.get("/showtimes", async function (req, res) {
-    const theater = "eastside10.json";
-    const url = "http://localhost:3000/theaters/detail/" + theater;
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.json({ error: "No match found." });
+  }
+});
 
-    try {
-      const response = await fetch(url);
-      const result = await response.json();
-      console.log(result);
-      res.json(result);
-    } catch (error) {
-      console.error(error);
-      res.json({ error: error.message });
-    }
-  });
-} else {
-  router.get("/", async function (req, res, next) {
-    const zip = req.query.zipCode;
-    const url =
-      "https://flixster.p.rapidapi.com/theaters/list?zipCode=" +
-      zip +
-      "&radius=50";
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": process.env.FLIXSTER_API_KEY,
-        "X-RapidAPI-Host": "flixster.p.rapidapi.com",
-      },
-    };
+// router.get("/showtimes", async function (req, res) {
+//   const theater = "eastside10.json";
+//   const url = "http://localhost:3000/theaters/detail/" + theater;
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log(result);
-      res.json(result);
-    } catch (error) {
-      console.error(error);
-      res.json({ error: error.message });
-    }
-  });
+//   try {
+//     const response = await fetch(url);
+//     const result = await response.json();
+//     console.log(result);
+//     res.json(result);
+//   } catch (error) {
+//     console.error(error);
+//     res.json({ error: error.message });
+//   }
+// });
 
-  router.get("/showtimes", async function (req, res) {
-    const theater = req.query.id;
-    const url = "https://flixster.p.rapidapi.com/theaters/detail?id=" + theater;
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": process.env.FLIXSTER_API_KEY,
-        "X-RapidAPI-Host": "flixster.p.rapidapi.com",
-      },
-    };
+// router.get("/showtimes", async function (req, res) {
+//   const theater = req.query.id;
+//   const url = "https://flixster.p.rapidapi.com/theaters/detail?id=" + theater;
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": process.env.FLIXSTER_API_KEY,
+//       "X-RapidAPI-Host": "flixster.p.rapidapi.com",
+//     },
+//   };
 
-    if (theater == "nx9fDyfpDcGvFwX")
-      try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
-        res.json(result);
-      } catch (error) {
-        console.error(error);
-        res.json({ error: error.message });
-      }
-  });
-}
+//   if (theater == "nx9fDyfpDcGvFwX")
+//     try {
+//       const response = await fetch(url, options);
+//       const result = await response.json();
+//       console.log(result);
+//       res.json(result);
+//     } catch (error) {
+//       console.error(error);
+//       res.json({ error: error.message });
+//     }
+// });
 
 module.exports = router;
